27ded5a13bfb33e5f3712dc568e376c42ed50849
Wir3s <113479857+Wir3s@users.noreply.github.com>
2022-11-13 11:13:01 -0600

    Buttons created, values assigned, successes celebrated, more to go

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // Declare global variables
 
 var timerElement = document.querySelector(".timer-count");
@@ -6,13 +5,10 @@ var startButton = document.querySelector(".start-button");
 var highScore = document.querySelector(".high-score");
 var questionField = document.querySelector(".question-box");
 var answerField = document.querySelector(".answer-box");
-var choicesList = document.querySelector(".oL")
 
-// Create question field or array or something!
+// Create question field
 var questionTheFirst = document.createElement("p");
-// Create list elements 
-var answerItem = document.createElement("li");
-
+// Create list elements
 
 var randomQuestion = "";
 var isComplete = false;
@@ -21,61 +17,58 @@ var timerCount;
 var score = 0;
 
 // Array(s) of questions and answers, and correct answer noted
-// Option 1
-// var questionsAnswers = [["question1", ["answer1", "answer2", "answer3", "answer4"]], ["question2", ["answer1", "answer2", "answer3", "answer4"]], ["question3", ["answer1", "answer2", "answer3", "answer4"]], ["question4", ["answer1", "answer2", "answer3", "answer4"]] ];
-// Option 2
 
 var questionsAnswers = [
   {
     question: "question 1",
     choices: ["option1", "option2", "option3", "option4"],
-    answer: "option3"
+    answer: "option3",
   },
- {
+  {
     question: "question 2",
     choices: ["option1", "option2", "option3", "option4"],
-    answer: "option1"
+    answer: "option1",
   },
   {
     question: "question 3",
     choices: ["option1", "option2", "option3", "option4"],
-    answer: "option1"
+    answer: "option1",
   },
   {
     question: "question 4",
     choices: ["option1", "option2", "option3", "option4"],
-    answer: "option1"
-}]
+    answer: "option1",
+  },
+];
 
 /*
-And in your HTML you can have a div for questions and a div for the multiple choice answers.
-you will place the questions.question into the textcontent of the div for questions and question[i].answers[k] goes into the textcontent of the div for multiple choice answers.
+var currentQuestionIndex = 0
+var currentQuestionObj = questionArrays[currentQuestionIndex]
+and then when you click on a choice button you could curentQuestionIndex++
+next time you call the function to get a new question the currentQuestionObj will be the next object in the array
 */
-
 
 // Called when page loads, looks for any existing high scores
 function init() {
-    getHighScores();
+  getHighScores();
 }
 
 // The startQuiz function is called when the start button is clicked
 function startQuiz() {
-    timerCount = 90;
-    // Hide start button when quiz begins
-    startButton.style.display = "none";
-    selectQuestion()
-    startTimer()
-    }
-
-console.log(timerElement);
+  timerCount = 90;
+  // Hide start button when quiz begins
+  startButton.style.display = "none";
+  selectQuestion();
+  startTimer();
+}
 
 //  Timer begins counting down when start button is pressed
 //  TImer will need to lose extra time when an incorrect answer is given
 function startTimer() {
-    // Set timer
-    timer = setInterval(function() {
-        timerCount--;
-        timerElement.textContent = timerCount;
+  // Set timer
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
     /* Set condition if all questions have been answered
          if (timerCount >=0) {
              if (isComplete && timerCount > 0) {
@@ -85,57 +78,55 @@ function startTimer() {
             }
 
     */
-         
-         // Tests if time has run out
-            if (timerCount === 0) {
-            // Clear interval
-            clearInterval(timer);
-            // Do something else, display score, prompt initials, etc
-             }
-    }, 1000);
+
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clear interval
+      clearInterval(timer);
+      // Do something else, display score, prompt initials, etc
+    }
+  }, 1000);
 }
 
 // Select a question that appears when ‘start button’ is pressed
-   
+
 function selectQuestion() {
-// Randomly select a question
- 
-    var choice1 = Math.floor(Math.random() * questionsAnswers.length);
-    var randomQuestion = questionsAnswers[choice1];
-    questionsAnswers.splice(randomQuestion, 1);
-    console.log(randomQuestion);
-    questionTheFirst.textContent = randomQuestion.question;
-    console.log(questionField);
-    questionField.appendChild(questionTheFirst);
+  // Randomly select a question
+  var choice1 = Math.floor(Math.random() * questionsAnswers.length);
+  var randomQuestion = questionsAnswers[choice1];
+  questionsAnswers.splice(randomQuestion, 1);
+  questionTheFirst.textContent = randomQuestion.question;
+  questionField.appendChild(questionTheFirst);
 
-// Display Choices
-    choicesList.innerHTML = "";
-    choicesLength = randomQuestion.choices.length;
-            for (var i = 0; i < choicesLength; i++) {
-                var listGen = randomQuestion.choices[i];
+  // Display Choices
+  choicesLength = randomQuestion.choices.length;
+  for (var i = 0; i < choicesLength; i++) {
+    var listGen = randomQuestion.choices[i];
+    var correctAnswer = randomQuestion.answer;
+    // Creates buttons from answer choices
+    var answerItem = document.createElement("button");
+    answerItem.textContent = listGen;
+    // set attributes on buttons
+    answerItem.setAttribute("name", listGen);
 
-                answerItem.textContent = listGen;
-                choicesList.appendChild(answerItem);
-                }
-              
-                console.log(answerItem);
-                console.log(randomQuestion.choices.length);
+    // Check if answer is correct, set value based on response
+    if (correctAnswer === answerItem.name) {
+      answerItem.setAttribute("value", true);
+    } else {
+      answerItem.setAttribute("value", false);
     }
-   
-   
-    
-    
+    // Adds buttons to answer container
+    answerField.appendChild(answerItem);
+    // Add event listener to buttons
+    answerItem.addEventListener("click", function () {
+      // if button clicked has value of 'true'
+      // if button clicked has value of 'false'
+      console.log(answerItem);
+    });
+  }
+}
 
 // A listener needs to watch for answers to be clicked on
-
-// Create responses for correct and incorrect answers
-
-// Attaches event listener to start button to call startQuiz function on click
-
-startButton.addEventListener("click", startQuiz);
-console.log(startButton);
-console.log(startTimer);
-
 
 // Create a condition when the timer reaches zero or all questions have been answered: score is shown, initials can be entered
 
@@ -150,3 +141,7 @@ console.log(startTimer);
 }
 
 */
+
+// Attaches event listener to start button to call startQuiz function on click
+
+startButton.addEventListener("click", startQuiz);
